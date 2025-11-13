21a2f1cd73575a4b8492f762c8e8b17ad05f5087
textilevikhyat-tech <textilevikhyat@gmail.com>
2025-11-13 14:03:55 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  coins: { type: Number, default: 100 },
-  createdAt: { type: Date, default: Date.now }
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+  username: {type:String, required:true, unique:true},
+  password: {type:String, required:true}
 });
-
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User",UserSchema);

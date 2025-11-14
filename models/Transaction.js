3f9e882fc2153f252c3a31d70e9eb04901f67576
textilevikhyat-tech <textilevikhyat@gmail.com>
2025-11-14 10:05:53 +0530

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,11 +1,9 @@
-// backend/models/Transaction.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const WalletSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   coins: { type: Number, default: 100 },
-  isAdmin: { type: Boolean, default: false },
-  createdAt: { type: Date, default: Date.now }
+  isAdmin: { type: Boolean, default: false }
 });
 
-module.exports = mongoose.model('Wallet', WalletSchema);
+module.exports = mongoose.model("Wallet", WalletSchema);

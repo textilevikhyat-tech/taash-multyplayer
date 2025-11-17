758741a51c13e46698f52929e952a58abd1cb01b
textilevikhyat-tech <textilevikhyat@gmail.com>
2025-11-17 13:38:47 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import React, { useState } from "react";
 import Table from "./components/Table";
+import Login from "./components/Login";
 
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/table" />} />
-      <Route path="/table" element={<Table />} />
-    </Routes>
-  );
+export default function App(){
+  const [user, setUser] = useState(() => {
+    const username = localStorage.getItem("username");
+    const token = localStorage.getItem("token");
+    return token && username ? { username, token } : null;
+  });
+
+  return user ? <Table user={user} /> : <Login onLogin={(u,t)=> setUser({ username:u, token:t })} />;
 }
-
-export default App;

6f0061d67139384edcc2cb203b3169e876dd6eca
textilevikhyat-tech <textilevikhyat@gmail.com>
2025-11-17 13:36:55 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "../public", // build output directly to server/public
-    emptyOutDir: false
-  }
+  plugins: [react()]
 });

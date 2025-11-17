e9fe621894bd304286999dc65dc309aac787e5e8
textilevikhyat-tech <textilevikhyat@gmail.com>
2025-11-17 12:41:42 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,11 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
